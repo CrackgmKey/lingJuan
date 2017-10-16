fa6edf204adf547b313fbadac5e607cbf8d0fcc5
CrackgmKey <31724772+CrackgmKey@users.noreply.github.com>
2017-10-16 13:48:40 +0800

    Update MainActivity.java

--- lingjuaner/src/main/java/com/lingjuan/app/activity/MainActivity.java
+++ lingjuaner/src/main/java/com/lingjuan/app/activity/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         ExampleApplication.AppContent = this;

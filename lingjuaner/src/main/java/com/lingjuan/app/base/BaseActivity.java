b2b6017271a06b04f9f0f648ebf5fccd2ee36b22
huitao <huitao@163.com>
2018-03-05 13:04:42 +0800

    修复因为父布局未引用导致的崩溃问题

--- lingjuaner/src/main/java/com/lingjuan/app/base/BaseActivity.java
+++ lingjuaner/src/main/java/com/lingjuan/app/base/BaseActivity.java
@@ -27,7 +27,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.base_title);
+        setContentView(getLayout());
         //输入框
         relativeLayout = (RelativeLayout) findViewById(R.id.include11dsw);
         //文字
